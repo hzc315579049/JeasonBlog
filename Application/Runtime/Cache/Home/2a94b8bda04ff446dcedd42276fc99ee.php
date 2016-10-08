6397145bf01f71221b620bb2a7b9f13b2cf78efe
Jeason_Huang <315579049@qq.com>
2016-10-08 19:16:53 +0800

    commit-1
    build project

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>主页</title>
+		<link rel="stylesheet" type="text/css" href="../Public/js/jquery.min.js"/>
+	</head>
+	<body>
+		hello!php!
+	</body>
+</html>
